cf4f1bd60c30852099dec2095e21c013886c1303
ehubin <ehubin@NCEL28991.iis.amadeus.net>
2016-10-14 09:25:22 +0200

    test fix

--- src/test.java
+++ src/test.java
@@ -4,12 +4,12 @@ public class test {
 
 	public static void main(String[] args) {
 		//Sorti 9143 135 -222 -162 0
-		UnleashTheGeek.Pod p=new UnleashTheGeek.Pod();
+		player1.Pod p=new player1.Pod();
 		p.x=400;
 		p.y=7412;
 		p.vx=38;
 		p.vy=165;
-		UnleashTheGeek.Action a = new UnleashTheGeek.Action(2.96759828703572,100);
+		player1.Action a = new player1.Action(2.96759828703572,100);
 		
 		p.move(a, null);
 	}
