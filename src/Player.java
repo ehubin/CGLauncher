fc0805a853756722e9a21cdd157e011bc5d82fd9
ehubin <ehubin@NCEL28991.iis.amadeus.net>
2017-09-21 11:02:30 +0200

    utg17 begin

---
+++ src/Player.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        State st=new State();
+        int planetCount = in.nextInt();
+        st.readEdges(in);
+
+        // game loop
+        while (true) {
+            for (int i = 0; i < planetCount; i++) {
+                st.planets[i].update(in);
+            }
+
+            // Write an action using System.out.println()
+            // To debug: System.err.println("Debug messages...");
+
+            System.out.println("0");
+            System.out.println("0");
+            System.out.println("0");
+            System.out.println("0");
+            System.out.println("0");
+            System.out.println("0");
+        }
+    }
+    
+    static class State {
+    	Planet[] planets;
+    	int nbP,nbE;
+    	int [][] edges;
+    	void readEdges(Scanner in) {
+    		nbP=in.nextInt();
+    		planets=new Planet[nbP];
+    		for(int i=0;i<nbP;++i) planets[i]=new Planet();
+    		nbE=in.nextInt();
+    		edges = new int[nbE][2];
+    		for(int i=0;i<nbE;++i) {
+    			edges[i][0]=in.nextInt();
+    			edges[i][1]=in.nextInt();
+    		}
+    	}
+    }
+    
+    static class Planet {
+    	int[] unit= new int[2];
+		int[] tolerance=new int[2];
+    	Planet() {
+    	}
+    	void update(Scanner in) {
+    		unit[0]=in.nextInt();
+    		tolerance[0]=in.nextInt();
+    		unit[1]=in.nextInt();
+    		tolerance[1]=in.nextInt();
+    		in.nextInt(); //ignore canAssign
+    	}
+    }
+}
---
+++ src/utg17State.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+
+public class utg17State implements GameState {
+
+	@Override
+	public int getResult() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getStateStr(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getInitStr(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void readActions(Scanner s, int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int resolveActions() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void startTurn() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public GameState save() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
